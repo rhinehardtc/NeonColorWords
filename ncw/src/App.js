7e5ada6e7883f2cff120e2dff5720ff7acfd5265
rhinehardtc <rhinehardtc@gmail.com>
2021-02-28 01:06:45 -0600

    Changed pink to magenta and added auxclick and prevented context menu

--- ncw/src/App.js
+++ ncw/src/App.js
@@ -2,17 +2,26 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [word, setWord] = useState('word');
-  const [hue, setHue] = useState('white');
+  const [word, setWord] = useState('white'); //For the word that is displayed
+  const [hue, setHue] = useState('white'); //For the color of the displayed text
 
-  const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'cyan', 'pink'];
+  const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'cyan', 'magenta'];
   const selectRandomColor = () => { return colors[Math.floor(Math.random() * colors.length)] };
 
   const style = {'color': hue}
 
   return (
-    <div className="App">
-      <h1 style={style} onClick={() => {setWord(selectRandomColor()); setHue(selectRandomColor())}}>{word}</h1>
+    <div 
+        className="App" 
+        onClick={() => {setWord(selectRandomColor()); setHue(selectRandomColor())}} 
+        onAuxClick={(e) => {
+            e.preventDefault();
+            setWord(selectRandomColor()); setHue(selectRandomColor());
+          }
+        }
+        onContextMenu={(e) => {e.preventDefault()}}
+    >
+      <h1 style={style}>{word}</h1>
     </div>
   );
 }
