8891498ef54aa22a7f6c95c9ae03afdd23f40a19
rhinehardtc <rhinehardtc@gmail.com>
2021-02-28 19:37:08 -0600

    added score and corresponding styling, plus comments

--- ncw/src/App.js
+++ ncw/src/App.js
@@ -4,16 +4,25 @@ import './App.css';
 function App() {
   const [word, setWord] = useState('white'); //For the word that is displayed
   const [hue, setHue] = useState('white'); //For the color of the displayed text
+  const [score, setScore] = useState(0); //The score to be incremented upon successful matching or rejection of hue and word
 
+  //List of colors to be randomly selected for both hue and word in the selectRandomColor function
   const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'cyan', 'magenta'];
+  //Select a color from the colors array at random based on index
   const selectRandomColor = () => { return colors[Math.floor(Math.random() * colors.length)] };
 
-  const style = {'color': hue, 'text-shadow': `0 0 30px ${hue}`}
+  //Styling used for the h1. It uses the hue set in state.
+  const style = {'color': hue, 'text-shadow': `0 0 6vmin ${hue}`};
+  //Styling used for the score counter
+  const scoreStyle = {'color': 'white', 'text-shadow': '0 0 4vmin white', 'margin-left': '2vw', 'align-self': 'flex-start'}
 
   return (
     <div 
         className="App" 
-        onClick={() => {setWord(selectRandomColor()); setHue(selectRandomColor())}} 
+        onClick={() => {
+            setWord(selectRandomColor()); setHue(selectRandomColor());
+          }
+        } 
         onAuxClick={(e) => {
             e.preventDefault();
             setWord(selectRandomColor()); setHue(selectRandomColor());
@@ -21,6 +30,7 @@ function App() {
         }
         onContextMenu={(e) => {e.preventDefault()}}
     >
+      <h3 style={scoreStyle}>Score: {score}</h3>
       <h1 style={style}>{word}</h1>
     </div>
   );
