7041f267ed3ccd68ca63714226666bb4176dd782
rhinehardtc <rhinehardtc@gmail.com>
2021-03-04 19:26:34 -0600

    Added end screen and accompmodified conditional render in App. Moved setScore(0) to EndScreen Component.

--- ncw/src/App.js
+++ ncw/src/App.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import EndScreen from './EndScreen';
 import StartScreen from './StartScreen';
 
 function App() {
-  const [start, setStart] = useState(false); //Determines if game is started. Passed as props to StartScreen Component.
+  const [start, setStart] = useState(false); //Determines if game is started. Passed as props to StartScreen & EndScreen Components.
+  const [end, setEnd] = useState(false); //Determines if game is ended. Passed as props to StartScreen & EndScreen Components.
   const [word, setWord] = useState('white'); //For the word that is displayed
   const [hue, setHue] = useState('white'); //For the color of the displayed text
   const [score, setScore] = useState(0); //The score to be incremented upon successful matching or rejection of hue and word
@@ -22,34 +24,49 @@ function App() {
   // isNotMatch does the same thing in reverse for onAuxClick
   // These functions will eventually need to determine if the game is reset, including seleciting to re-render the eventual start/end screen
   const isMatch = () => {
-    hue === word ? setScore(score + 1) : setScore(0);
+    if(hue === word) setScore(score + 1);
+    else {setStart(false); setEnd(true)};
   };
   const isNotMatch = () => {
-    hue !== word ? setScore(score + 1) : setScore(0);
+    if(hue !== word) setScore(score + 1);
+    else {setStart(false); setEnd(true)};
   };
 
+  //Decides whether to render the start screen, end screen, or main game interface based on start and end state boolean values.
+  //Exclusively returned in App return statement.
+  const decideGamePhase = () => {
+    if(start){
+      return(
+        <div 
+          className="App" 
+          onClick={() => {
+              isMatch();
+              setWord(selectRandomColor()); setHue(selectRandomColor());
+            }
+          } 
+          onAuxClick={(e) => {
+              e.preventDefault();
+              isNotMatch();
+              setWord(selectRandomColor()); setHue(selectRandomColor());
+            }
+          }
+          onContextMenu={(e) => {e.preventDefault()}}
+          >
+          <h3 style={scoreStyle}>Score: {score}</h3>
+          <h1 style={wordStyle}>{word}</h1>
+        </div>
+      )
+    } else if(end){
+      return <EndScreen score={score} setScore={setScore} setStart={setStart} setEnd={setEnd}/>
+    } else {
+      return <StartScreen setStart={setStart}/>
+    }
+  }
+
   return (
-    start ?
-    <div 
-        className="App" 
-        onClick={() => {
-            isMatch();
-            setWord(selectRandomColor()); setHue(selectRandomColor());
-          }
-        } 
-        onAuxClick={(e) => {
-            e.preventDefault();
-            isNotMatch();
-            setWord(selectRandomColor()); setHue(selectRandomColor());
-          }
-        }
-        onContextMenu={(e) => {e.preventDefault()}}
-    >
-      <h3 style={scoreStyle}>Score: {score}</h3>
-      <h1 style={wordStyle}>{word}</h1>
-    </div>
-    :
-    <StartScreen setStart={setStart}/>
+    <>
+      {decideGamePhase()}
+    </>
   );
 }
 
---
+++ ncw/src/EndScreen.js
@@ -0,0 +1,15 @@
+const EndScreen = (props) => {
+    const {score, setScore, setStart, setEnd} = props;
+
+    //Converts score to a cents on the dollar value and returns a statement including the wage
+    const earnedWage = () => {return `Thank you for your services. You have earned $${score/100.00}.`}
+
+    return(
+        <div className="App StartScreen">
+            <h3>{earnedWage()}</h3>
+            <h4 onClick={() => {setScore(0); setStart(false); setEnd(false)}}>Please click here to end your shift.</h4>
+        </div>
+    )
+}
+
+export default EndScreen;
