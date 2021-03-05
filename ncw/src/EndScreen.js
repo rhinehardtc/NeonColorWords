6d4c2ff1933f5daaa20dbee89387f6975c0020e7
rhinehardtc <rhinehardtc@gmail.com>
2021-03-04 19:59:49 -0600

    fixed decimal place issue in monetary final score shown

--- ncw/src/EndScreen.js
+++ ncw/src/EndScreen.js
@@ -2,7 +2,7 @@ const EndScreen = (props) => {
     const {score, setScore, setStart, setEnd} = props;
 
     //Converts score to a cents on the dollar value and returns a statement including the wage
-    const earnedWage = () => {return `Thank you for your services. You have earned $${score/100.00}.`}
+    const earnedWage = () => {return `Thank you for your services. You have earned $${(score/100.00).toFixed(2)}.`}
 
     return(
         <div className="App StartScreen">
